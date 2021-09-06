29e4b34ab67395d4eb6a1fd5fe724bbf47cd1f53
Mazurka Zita <tmazurkazita@gmail.com>
2021-09-06 14:13:36 +0200

    add feladat3

--- testproject/test_k3.py
+++ testproject/test_k3.py
@@ -3,22 +3,23 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 
 options = webdriver.ChromeOptions()
-#options.add_argument('--headless')
-#options.add_argument('--disable-gpu')
+# options.add_argument('--headless')
+# options.add_argument('--disable-gpu')
 driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 URL = "https://ambitious-sky-0d3acbd03.azurestaticapps.net/k3.html"
 
 # Oldal betöltése
 driver.get(URL)
 
-#Lokátorok
+# Lokátorok
 title = driver.find_element_by_id("title")
 
-#Tesztadatok
-title_data = ["abcd1234", "teszt233@", "abcd" ]
+# Tesztadatok
+title_data = ["abcd1234", "teszt233@", "abcd"]
 excepted_error_msg = ["", "Only a-z and 0-9 characters allewed",
                       "Title should be at least 8 characters; you entered 4."]
 error_msg = driver.find_element_by_xpath("/html/body/form/span")
+
 
 def test_valid_fill():
     """Helyes kitöltés esete:
@@ -42,8 +43,3 @@ def test_short_text_data():
     title.send_keys(title_data[2])
 
     assert error_msg.text == excepted_error_msg[2]
-
-
-
-
-
